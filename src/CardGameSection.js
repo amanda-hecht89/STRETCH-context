8cb0028fbcc6ec34f8a2a384a4979ab3b043bfb4
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-11 18:07:24 -0600

    app js edit

--- src/CardGameSection.js
+++ src/CardGameSection.js
@@ -9,20 +9,5 @@ export default function CardGameSection() {
     deck, playerOneHand, setPlayerOneHand, selectedCard, setSelectedCard, playerTwoHand,
     setPlayerTwoHand, playerThreeHand, setPlayerThreeHand, setFrom, to, setTo,
   } = useCardGameContext();
-  return (
-    <div className="section">
-      <section>
-        {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player to={to} player={setPlayerOneHand} hand={playerOneHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
-        <Player to={to} player={setPlayerTwoHand} hand={playerTwoHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
-        <Player to={to} player={setPlayerThreeHand} hand={playerThreeHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
-        <CardList cards={deck} selectedCard={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} player={'deck'} />
-      </section>
-      <section>
-        {
-          selectedCard && <ExecutePassButton />
-        }
-      </section>
-    </div>
-  );
+  
 }
