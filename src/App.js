38c1298156390a96bf40582e76cd1c1f4d0f756f
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-11 19:02:23 -0600

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
   } = useCardGameContext();
   return (
     <div className="App">
+      <header>
+        <h1>WELCOME</h1>
+      </header>
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
         <Player player={1} hand={playerOneHand} />
