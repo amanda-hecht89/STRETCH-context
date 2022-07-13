40b8b051c1d259647d556f9a51c10c98596d148e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-12 18:09:31 -0600

    commented out test

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import './App.css';
 import Player from './Player';
 import CardList from './CardList';
 import ExecutePassButton from './ExecutePassButton';
-import { useCardGameContext } from './CardGameProvider';
+import CardGameProvider, { useCardGameContext } from './CardGameProvider';
 
 function App() {
   const {
     selectedCard, deck, playerOneHand, playerTwoHand, playerThreeHand
   } = useCardGameContext();
   return (
-    <div className="App">
-      <header>
-        <h1>WELCOME</h1>
-      </header>
-      <section>
-        {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player player={1} hand={playerOneHand} />
-        <Player player={2} hand={playerTwoHand} />
-        <Player player={3} hand={playerThreeHand} />
-        <CardList cards={deck} player={'deck'} />
-      </section>
-      <section>
-        {
-          selectedCard && <ExecutePassButton />
-        }
-      </section>
-    </div>
+    <CardGameProvider>
+      <div className="App">
+        <header>
+          <h1>WELCOME</h1>
+        </header>
+        <section>
+          {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
+          <Player player={1} hand={playerOneHand} />
+          <Player player={2} hand={playerTwoHand} />
+          <Player player={3} hand={playerThreeHand} />
+          <CardList cards={deck} player={'deck'} />
+        </section>
+        <section>
+          {
+            selectedCard && <ExecutePassButton />
+          }
+        </section>
+      </div>
   );
-}
+    </CardGameProvider>
+  }
 export default App;
